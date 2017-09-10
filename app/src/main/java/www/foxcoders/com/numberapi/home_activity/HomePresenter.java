02c4537165356ee21e8d18c36ac8076cffc6942d
rayhan0123 <64557679ray>
2017-09-10 13:27:50 +0600

    Repository Added

--- app/src/main/java/www/foxcoders/com/numberapi/home_activity/HomePresenter.java
+++ app/src/main/java/www/foxcoders/com/numberapi/home_activity/HomePresenter.java
@@ -1,5 +1,7 @@
 package www.foxcoders.com.numberapi.home_activity;
 
+import android.content.Context;
+
 import www.foxcoders.com.numberapi.data.HomeRepository;
 import www.foxcoders.com.numberapi.data.ResponseResult;
 
@@ -7,8 +9,34 @@ import www.foxcoders.com.numberapi.data.ResponseResult;
  * Created by Ray on 9/9/2017.
  */
 
-public class HomePresenter implements  HomeContract.DateAction, HomeContract.NumberAction, HomeContract.TriviaAction {
-    HomeRepository repository=new HomeRepository();
+public class HomePresenter implements HomeContract.DateAction, HomeContract.NumberAction, HomeContract.TriviaAction {
+
+    private  Context context;
+    private HomeContract.DateView dateView;
+    private HomeContract.TriviaView triviaView;
+    private HomeContract.NumberView numberView;
+
+    public HomePresenter(Context context,  HomeContract.DateView dateView )
+    {
+        this.context=context;
+        this.dateView=dateView;
+
+    }
+    public HomePresenter(Context context,  HomeContract.TriviaView triviaView )
+    {
+        this.context=context;
+        this.triviaView=triviaView;
+
+    }
+    public HomePresenter(Context context,  HomeContract.NumberView numberView )
+    {
+        this.context=context;
+        this.numberView=numberView;
+
+    }
+
+    private HomeRepository repository = new HomeRepository();
+
     @Override
     public void getDateData() {
 
